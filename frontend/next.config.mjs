75628d2ea7490d8ad6184d03052f2d413a6aad92
Navghare__vaibhav <75133602+vaibhavnavghare4253@users.noreply.github.com>
2025-11-05 13:32:28 +0530

    check

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,6 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
